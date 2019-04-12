55faef9f06773dd75809196b2cbccba649c22f9c
kaiyuexu <kaiyuexu09@gmail.com>
2019-04-12 21:51:41 +0200

    Update 2019-04-12 21:51:11

--- app/src/main/java/com/example/designforinteraction/MainActivity.java
+++ app/src/main/java/com/example/designforinteraction/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.designforinteraction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvTeamA = findViewById(R.id.tvTeamA);
+        tvTeamA.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(final View v){
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
+
+
