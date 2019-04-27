8d47bffa90d9b8df86094df39b3df1b5d73b87fc
kaiyuexu <kaiyuexu09@gmail.com>
2019-04-27 19:58:28 +0200

    Listener/Motion Sensor/Final concept

--- app/src/main/java/com/example/designforinteraction/MainActivity.java
+++ app/src/main/java/com/example/designforinteraction/MainActivity.java
@@ -1,28 +1,70 @@
 package com.example.designforinteraction;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    private SensorManager sensorManager;
+    private int i = 0;
+    private static ArrayList<String> resList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvTeamA = findViewById(R.id.tvTeamA);
-        tvTeamA.setOnClickListener(new View.OnClickListener(){
-            @Override
-                    public void onClick(final View v){
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(intent);
+        TextView tvSYG= findViewById(R.id.tvSYG);
+        final TextView tvList = findViewById(R.id.tvList);
+
+        tvSYG.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent menuIntent = new Intent(MainActivity.this, Main2Activity.class);
+                        startActivity(menuIntent);
+                    }
+                });
+
+        if (getIntent().hasExtra("total_points")) {
+            Bundle bundle = getIntent().getExtras();
+            String points_i = bundle.getString("total_points");
+            resList.add(points_i);
+
+            String resStr = "";
+            int tot = 0;
+            i = 1;
+            for (String pts : resList) {
+                resStr += "Round " + i++ + ": " + pts + "\n";
+                tot += Integer.parseInt(pts);
             }
 
+            resStr += "Total: " + tot;
 
-        });
+
+            tvList.setText(resStr);
+        }
+
     }
+
+
 }
+
+
+
+
+
 
 
